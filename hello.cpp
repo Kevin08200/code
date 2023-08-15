22291eef602cd5452b1eb182e779dcb6acba115a
Kevin <2424805564@qq.com>
2023-08-15 23:34:04 +0800

    git init

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-07-30 23:33:53
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-11-09 23:08:35
+ * @FilePath: \Code\hello.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+#define UINT32_MY_MAX 0xFFFFFFFFu
+
+int main()
+{
+   uint32_t max = UINT32_MY_MAX;
+
+   cout << max << endl;
+   system("pause");
+   return 0;
+}
